21e7cc0682b3ed2c3faea4461ed03bf48deb1628
Patrick B Gibson <patrick@fadeover.org>
2023-05-23 13:52:30 -0700

    Mohammed’s changes

--- QTFController/QTFController.ino
+++ QTFController/QTFController.ino
@@ -1,6 +1,18 @@
 #include "SerialTransfer.h"
 #include <AccelStepper.h>
 
+//Notes
+ //********************************************************************************************************** 
+ //Any data that you're writing to the Serial port using Serial.println() 
+ //or Serial.print() in your sketch will be graphed in real time in the Serial Plotter. 
+ //********************************************************************************************************** 
+ //To convert the time from milliseconds to seconds, minutes, or hours, you can use the following equations: 
+ //Seconds: time (in seconds) = millis() / 1000 
+ //Minutes: time (in minutes) = millis() / (1000 * 60) 
+ //Hours: time (in hours) = millis() / (1000 * 60 * 60) 
+ //***********************************************************************************************************
+
+
 struct STRUCT {
   float temp1;
   float temp2;
@@ -11,44 +23,33 @@ struct STRUCT {
 
 SerialTransfer teensyToArduinoTransfer;
 
-//Nema 23
 const int dirPin1 = 2;
 const int stepPin1 = 3;
-AccelStepper linearStepper(AccelStepper::DRIVER, stepPin1, dirPin1); //controlled by driver board, 3 = step, 2 = direction
-
-bool motorTurning = false;
-
+AccelStepper linearStepper(AccelStepper::DRIVER, stepPin1, dirPin1);
 
 void setup() {
   SerialUSB.begin(9600);
   Serial1.begin(9600);
   teensyToArduinoTransfer.begin(Serial1);
 
-  linearStepper.setMaxSpeed(200); //max speed set in pul/rev
-  linearStepper.setSpeed(200); //speed in steps per second
+  linearStepper.setMaxSpeed(800); 
+  linearStepper.setSpeed(800);
   linearStepper.setCurrentPosition(0);
-  linearStepper.setAcceleration(200);
-  linearStepper.runToNewPosition(200*100);
+  linearStepper.setAcceleration(300);
+  linearStepper.moveTo(((200*100)/4)*300); 
 
+  SerialUSB.println("Time(s),Temp1,Temp2,Temp3,Temp4,Temp5,StepperPosition,StepperSpeed,StepperAcceleration,StepperDistanceToGo,StepperDirection,StepperTargetPosition");
   delay(500);
 }
 
 void loop() {
-
-  SerialUSB.println("Starting Loop");
-
-  // linearStepper.runSpeed();
+  linearStepper.run(); // Stepper motor run command moved here
 
   if(teensyToArduinoTransfer.available()) {
-
-    SerialUSB.println("Got new data.");
-
     uint16_t recSize = 0;
     recSize = teensyToArduinoTransfer.rxObj(data, recSize);
     
-
-    float time = millis();
-    SerialUSB.print(time);
+    SerialUSB.print(millis()/1000.0); # time printed in seconds
     SerialUSB.print(',');
     SerialUSB.print(data.temp1);
     SerialUSB.print(',');
@@ -59,20 +60,21 @@ void loop() {
     SerialUSB.print(data.temp4);
     SerialUSB.print(',');
     SerialUSB.print(data.temp5);
-    SerialUSB.print('\n');
-    
+    SerialUSB.print(',');
 
-    ////// Choose what temperature we want to send to the temperature controller
-
-
-    ////// Send data to the temperature controller
-
+    SerialUSB.print(linearStepper.currentPosition());
+    SerialUSB.print(',');
+    SerialUSB.print(linearStepper.speed());
+    SerialUSB.print(',');
+    SerialUSB.print(linearStepper.acceleration());
+    SerialUSB.print(',');
+    SerialUSB.print(linearStepper.distanceToGo());
+    SerialUSB.print(',');
+    SerialUSB.println(linearStepper.targetPosition());
 
 
   } else if(teensyToArduinoTransfer.status < 0) {
-
     SerialUSB.print("ERROR: ");
-
     if(teensyToArduinoTransfer.status == -1)
       SerialUSB.println(F("CRC_ERROR"));
     else if(teensyToArduinoTransfer.status == -2)
@@ -81,5 +83,5 @@ void loop() {
       SerialUSB.println(F("STOP_BYTE_ERROR"));
   }
 
-  delay(1000);
+  // delay(1000); // Commented this out to make stepper motor run more smoothly
 }
