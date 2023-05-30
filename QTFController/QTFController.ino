3bdc8e2d4d2236ac43449450e3448ec9e00d4a86
Patrick B Gibson <patrick@fadeover.org>
2023-05-30 09:46:31 -0700

    Latest changes with mohammed

--- QTFController/QTFController.ino
+++ QTFController/QTFController.ino
@@ -9,34 +9,35 @@
 
 int targetTemperature = 20;   // In degrees C. Min is 0, Max is 700.
 int runTime = 100;            // In minutes.
+const float DesiredPosition1 = -5.0; // Milimeters from current position
 
 // ------------ End Configuration Section ----------
 
 
-
-#define MAX485_DE 7         //DE Pin of Max485
-#define MAX485_RE_NEG 6     //RE Pin of Max485
+// Buttons
 #define GREEN_BUTTON_PIN 22
 #define RED_BUTTON_PIN 23
 #define LIMIT_SWITCH_START_SIDE 24
 #define LIMIT_SWITCH_END_SIDE 25
-
-#define MySerial Serial3
-#define PresentValueAddress 0x3E8
-
 int debounce = 50;
 bool pullup = true;
 bool invert = true;
-EasyButton startButton(GREEN_BUTTON_PIN, debounce, pullup, invert);
-EasyButton stopButton(RED_BUTTON_PIN, debounce, pullup, invert);
+EasyButton startButton(GREEN_BUTTON_PIN, debounce, pullup, false);
+bool startWasPressed = false;
+EasyButton stopButton(RED_BUTTON_PIN, debounce, pullup, false);
+bool stopWasPressed = false;
 EasyButton limitSwitchStart(LIMIT_SWITCH_START_SIDE, debounce, pullup, invert);
 EasyButton limitSwitchEnd(LIMIT_SWITCH_END_SIDE, debounce, pullup, invert);
 
-
+// Temp Controller
+#define MAX485_DE 7         //DE Pin of Max485
+#define MAX485_RE_NEG 6     //RE Pin of Max485
+#define MySerial Serial3
+#define PresentValueAddress 0x3E8
 ModbusMaster tempControllerMM;
-// ModbusSerial tempControllerMM(MySerial, 1, MAX485_DE);
 int presentValue = 0;
 
+// Teensy Serial Communication
 struct STRUCT {
   float temp1;
   float temp2;
@@ -44,14 +45,22 @@ struct STRUCT {
   float temp4;
   float temp5;
 } data;
-
 SerialTransfer teensyToArduinoTransfer;
 
+// Stepper Motor Driver
 const int dirPin1 = 2;
 const int stepPin1 = 3;
 AccelStepper linearStepper(AccelStepper::DRIVER, stepPin1, dirPin1);
-
 int desiredPosition = -1; // User defined desired position
+const int stepsPerRotation = 200;
+const int gearboxRatio = 100;
+const float leadOfScrew = 4.0;
+const float estimatedSpeed = 800.0; // Estimated speed in steps per second
+const float estimatedAcceleration = 300.0; // Estimated acceleration in steps per second squared
+unsigned long estimatedRunTime = 0; // Global variable for estimated run time
+unsigned long lastUpdateTime = 0; // Last time the estimated run time was updated
+const unsigned long updateInterval = 1000; // Update interval for estimated run time in milliseconds
+const float stepSize = 1.0 / (stepsPerRotation * gearboxRatio / leadOfScrew); // Step size in millimeters
 
 
 void readPresentValue() {
@@ -120,11 +129,14 @@ void setup() {
   Serial1.begin(9600);
   teensyToArduinoTransfer.begin(Serial1);
 
-  linearStepper.setMaxSpeed(800); 
+  linearStepper.setMaxSpeed(800);
   linearStepper.setSpeed(800);
   linearStepper.setCurrentPosition(0);
   linearStepper.setAcceleration(300);
+  int desiredPosition = ((stepsPerRotation * gearboxRatio) / leadOfScrew) * DesiredPosition1; // Calculate desired position in steps
+  linearStepper.moveTo(desiredPosition); // Move the stepper motor to the desired position
 
+  lastUpdateTime = millis(); // Initialize the last update time
   pinMode(MAX485_RE_NEG, OUTPUT);
   pinMode(MAX485_DE, OUTPUT);
 
@@ -150,43 +162,53 @@ void setup() {
   
   Scheduler.startLoop(loop2);
 
-  SerialUSB.println("Enter the desired position when ready:");
+  SerialUSB.println("Time(s),Temp1,Temp2,Temp3,Temp4,Temp5,StepperPosition(mm),StepperSpeed(mm/s),StepperAcceleration(mm/s^2),StepperDistanceToGo(mm),EstimatedRunTime(s)");
 }
 
-void loop() {
-  if(desiredPosition == -1 && SerialUSB.available() > 0) {
-    desiredPosition = SerialUSB.parseInt();
-    linearStepper.moveTo(((200*100)/4)*desiredPosition); 
-    SerialUSB.println("Time(s),Temp1,Temp2,Temp3,Temp4,Temp5,StepperPosition,DesiredPosition,TimeRemaining");
-    delay(500);
-  }
-  
-  linearStepper.run(); // Stepper motor run command moved here
+void loop() {  
 
+  if (stopWasPressed == false && startWasPressed == true) {
+    linearStepper.run(); // Stepper motor run command moved here
+  }
+
+  // Update the estimated run time at the specified interval
+  if (millis() - lastUpdateTime >= updateInterval) {
+    // Calculate estimated run time
+    float distanceToTravel = abs(linearStepper.distanceToGo());
+    float timeToReachPosition = sqrt(2.0 * distanceToTravel / estimatedAcceleration) + (distanceToTravel / estimatedSpeed);
+    estimatedRunTime = (unsigned long)(timeToReachPosition * 1000.0); // Convert to milliseconds
+
+    lastUpdateTime = millis(); // Update the last update time
+  }
+
+  // If we are able to get temperature data from the teensy thermocouple controller...
   if(teensyToArduinoTransfer.available()) {
     uint16_t recSize = 0;
     recSize = teensyToArduinoTransfer.rxObj(data, recSize);
     
-    SerialUSB.print(millis()/1000.0);
+       // Print the data values
+    SerialUSB.print(millis() / 1000.0, 2); // Print current time in seconds
     SerialUSB.print(',');
-    SerialUSB.print(data.temp1);
+    SerialUSB.print(data.temp1, 2); // Print temperature 1
     SerialUSB.print(',');
-    SerialUSB.print(data.temp2);
+    SerialUSB.print(data.temp2, 2); // Print temperature 2
     SerialUSB.print(',');
-    SerialUSB.print(data.temp3);
+    SerialUSB.print(data.temp3, 2); // Print temperature 3
     SerialUSB.print(',');
-    SerialUSB.print(data.temp4);
+    SerialUSB.print(data.temp4, 2); // Print temperature 4
     SerialUSB.print(',');
-    SerialUSB.print(data.temp5);
+    SerialUSB.print(data.temp5, 2); // Print temperature 5
     SerialUSB.print(',');
-    SerialUSB.print(linearStepper.currentPosition());
+    SerialUSB.print(linearStepper.currentPosition() * stepSize, 3); // Print stepper position in millimeters
     SerialUSB.print(',');
-    SerialUSB.print(desiredPosition);
+    SerialUSB.print(linearStepper.speed() * stepSize, 2); // Print stepper speed in millimeters per second
     SerialUSB.print(',');
-    int timeRemaining = desiredPosition - linearStepper.currentPosition();
-    SerialUSB.print(timeRemaining < 0 ? 0 : timeRemaining);
+    SerialUSB.print(linearStepper.acceleration() * stepSize, 2); // Print stepper acceleration in millimeters per second squared
     SerialUSB.print(',');
-    SerialUSB.println(presentValue);
+    SerialUSB.print(linearStepper.distanceToGo() * stepSize, 3); // Print stepper distance to go in millimeters
+    SerialUSB.print(',');
+    SerialUSB.print(estimatedRunTime / 1000.0, 3); // Print estimated run time in seconds
+    SerialUSB.println();
 
   } else if(teensyToArduinoTransfer.status < 0) {
     SerialUSB.print("ERROR: ");
@@ -203,19 +225,23 @@ void loop() {
 
 void startPressed() {
   SerialUSB.println("Start button has been pressed!");
-  readPresentValue(); // Debug Only
+  startWasPressed = true;
+  stopWasPressed = false;
 }
 
 void stopPressed() {
   SerialUSB.println("Stop button has been pressed!");
+  stopWasPressed = true;
 }
 
 void limitStartHit() {
   SerialUSB.println("Limit Start button has been pressed!");
+  stopWasPressed = true;
 }
 
 void limitEndHit() {
   SerialUSB.println("Limit End button has been pressed!");
+  stopWasPressed = true;
 }
 
 void loop2() {
